924b1008ff3a247e3baa9b75a4e779789e71dbef
zufius <achung0147@gmail.com>
2022-03-12 16:34:30 +0700

    First version updated: Nextjs deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
 	images: {
 		domains: ["cloudflare-ipfs.com", "etherscan.io"],
 	},
-	assetPrefix: "/ethereum-nextjs-boilerplate/",
-	basePath: "/ethereum-nextjs-boilerplate",
+	assetPrefix: "/",
+	basePath: "",
 };
--- src/pages/wallet.jsx
+++
@@ -1,8 +0,0 @@
-import Wallet from 'components/Wallet/Wallet';
-import React from 'react'
-
-function wallet() {
-    return <Wallet/>
-}
-
-export default wallet
